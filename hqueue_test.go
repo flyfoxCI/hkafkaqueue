17b32998ffbeb73ad739f5289838b9bd2192d2ae
yunding <zhanxiaohui2@gmail.com>
2019-10-14 10:11:28 +0800

    remove some errors when merge dev1

--- hqueue_test.go
+++ hqueue_test.go
@@ -2,12 +2,10 @@ package HKafkaQueue
 
 import (
 	"fmt"
-	"sync"
 	"testing"
-	"time"
 )
 
-var dataDir = "/tmp/hqueue"
+var dataDir = "/tmp/kqueue"
 var queueName = "test"
 
 func TestNewHQueue(t *testing.T) {
@@ -25,121 +23,41 @@ func TestQueueWrite(t *testing.T) {
 		t.Fatalf("create hqueue error %v", err)
 	}
 	for {
-		if hqueue.index.writeBlockNum > 100 {
-			break
-		}
 		_, err := hqueue.Offer([]byte("Bridgewater Associates AQR Capital Management Millennium Management Citadel Soros Fund Management Winton Capital Management D.E. Shaw& Co. enaissance Technologies LLC Two Sigma Paulson & Co."))
 		if err != nil {
 			t.Fatalf("write error :%v", err)
 			break
 		}
 		i = i + 1
+		if i > 1000000 {
+			hqueue.Close()
+			break
+		}
 	}
-	fmt.Println(hqueue.index.writeCounter)
+	fmt.Println(hqueue.producerIndex.counter)
 	fmt.Println(i)
-	hqueue.Sync()
 
 }
 
 func TestQueueRead(t *testing.T) {
-	hqueue, err := NewHQueue(queueName, dataDir)
+	hqueue, err := NewHQueue(queueName, dataDir, "p4")
 	var i = 0
 	if err != nil {
-		t.Fatalf("create hqueue error %v", err)
+		t.Fatalf("create hqueue error: %v", err)
 	}
 	for {
 		_, err := hqueue.Poll()
 		if err != nil {
-			break
+			continue
 		} else {
 			//fmt.Println(String(bytes))
 			i = i + 1
 		}
+		if i == 1000000 {
+			hqueue.Close()
+			break
+		}
 	}
-	fmt.Println(hqueue.index.readCounter)
+	fmt.Println(hqueue.consumerIndex.counter)
 	fmt.Println(i)
-	hqueue.Sync()
-}
-
-func BenchmarkWrite(t *testing.B) {
-	hqueue, _ := NewHQueue(queueName, dataDir)
-	var w sync.WaitGroup
-	w.Add(1)
-	write(hqueue, &w)
-}
-
-func TestRead(t *testing.T) {
-	fmt.Println(time.Now())
-	hqueue, _ := NewHQueue(queueName, dataDir)
-	var w sync.WaitGroup
-	w.Add(1)
-	read(hqueue, &w)
-	fmt.Println(time.Now())
-}
-
-func BenchmarkMultipleReadWrite(t *testing.B) {
-	defer func() {
-		fmt.Println("c")
-		if err := recover(); err != nil {
-			fmt.Println(err)
-		}
-	}()
-	hqueue, _ := NewHQueue(queueName, dataDir)
-	var w sync.WaitGroup
-	w.Add(2)
-	go read(hqueue, &w)
-	go write(hqueue, &w)
-	w.Wait()
-}
-
-func write(hqueue *HQueue, w *sync.WaitGroup) {
-	var i = 0
-	for {
-		_, err := hqueue.Offer([]byte("Bridgewater Associates AQR Capital Management Millennium Management Citadel Soros Fund Management Winton Capital Management D.E. Shaw& Co. enaissance Technologies LLC Two Sigma Paulson & Co." +
-			"Bridgewater Associates AQR Capital Management Millennium Management Citadel Soros Fund Management Winton Capital Management D.E. Shaw& Co. enaissance Technologies LLC Two Sigma Paulson & Co."))
-		if err != nil {
-			continue
-		}
-		i = i + 1
-		if i%100000000 == 0 {
-			//fmt.Printf("write msg count:%d",i)
-			//fmt.Println(hqueue)
-			//time.Sleep(time.Second*2)
-			w.Done()
-			break
-		}
-	}
-	//os.RemoveAll(dataDir+string(os.PathSeparator)+queueName)
-}
-
-func read(hqueue *HQueue, w *sync.WaitGroup) {
-	var i = 0
-	for {
-		//fmt.Println("read......")
-		b, err := hqueue.Poll()
-		_ = b //release memory or will cause out of range
-		if err != nil {
-			if _, ok := err.(*ReadZeroError); ok {
-				//fmt.Println("read Zero")
-				//time.Sleep(time.Second)
-				if hqueue.index.readCounter != 0 {
-					fmt.Println(hqueue.index.readCounter)
-					fmt.Println(i)
-				}
-				time.Sleep(time.Second)
-			} else {
-				fmt.Errorf("%s", err)
-			}
-		} else {
-			i = i + 1
-			//fmt.Printf("read msg i:%d :%s\n",i,string(msg))
-		}
-		if i == 100000000 {
-			fmt.Println("break")
-			w.Done()
-			break
-		}
-
-	}
-
 }
